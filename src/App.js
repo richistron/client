2eb855c546ce6dac01e6936751f8e6cd744ad5fd
Ricardo Rivas <richistron@gmail.com>
2018-07-08 21:41:37 -0500

    adds router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
-import DefaultLayout from './layouts/DefaultLayout';
+import React, { Component } from 'react';
+import Routes from './routes/Routes';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <DefaultLayout />
-      </div>
+        <div className="App">
+            <Routes />
+        </div>
     );
   }
 }
