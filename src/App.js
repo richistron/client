cac6f8c771dd580c0ec68e00ac70f4670250e134
Ricardo Rivas <richistron@gmail.com>
2018-07-08 19:00:31 -0500

    new app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import DefaultLayout from './layouts/DefaultLayout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <DefaultLayout />
       </div>
     );
   }
