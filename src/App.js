7a0daee44f808df18bb1c1a2adebe154cbea5fe3
Ricardo Rivas <richistron@gmail.com>
2018-07-22 21:04:26 -0500

    fix app code style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import React, { Component } from 'react';
+import React from 'react';
 import Routes from './routes';
 import store from './reducers/store';
 import { Provider } from 'react-redux';
 
-// eslint-disable-next-line
-class App extends Component {
-  render() {
-    return <Provider store={store}><Routes /></Provider>;
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+);
 
 export default App;
